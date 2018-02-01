a86257ca1ee97255c779105f9ec8e983d81ea6a2
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 14:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 function happyHolidays() {
   return "Happy Holidays!";
 }
+
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`;
+}
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
